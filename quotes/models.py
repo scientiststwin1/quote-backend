7044923ceb8851f7bb5b14d19141bc299e53a08d
Twin <scientists.twin1@gmail.com>
2023-12-04 16:30:42 +0400

    feature: add quote model

--- quotes/models.py
+++ quotes/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Quote(models.Model):
+    class Meta:
+        verbose_name = 'Quote'
+        verbose_name_plural = 'Quotes'
+        get_latest_by = 'created_at'
+
+    text = models.TextField()
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text
